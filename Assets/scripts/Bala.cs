3b46b4c6a720ac44a965d36eeb1678e2bfd215e9
EggsF0rAll <leonardo.rosas@uteca.edu.mx>
2025-10-22 19:42:39 -0600

    Gun extension

--- Assets/scripts/Bala.cs
+++ Assets/scripts/Bala.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
-
+using UnityEngine.Events;
 public class Bala : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float speed = 10f;
+    [SerializeField]
+
+    private float damage = 20f;
+    [SerializeField]
+    private UnityEvent<Transform> onHitEnemy;
+
+    private RigidBody rigidbody;
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<RigidBody>();
+
+    }
+ private void OnEnable()
+       {
+          rigidbody.linearVelocity = Transform.forward * speed;
+       }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Health enemyHealth = other.GetComponent<enemyHealth>();
+            if (enemyHealth != null)
+
+            {
+                enemyHealth.TakeDamage(damage);
+            }
+            SoundManager.instance.Play("hit_object");
+            onHitEnemy?.Invoke(Transform);
+            gameObject.SetActive(false);
+        }
+    }
+          private void OnDisable()
+    {
+        GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
